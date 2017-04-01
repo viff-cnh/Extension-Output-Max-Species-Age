5a7c8c202ffaa8ebef231999a8ab4698327ec641
aruzicka555 <austen.ruzicka@gmail.com>
2017-04-01 14:44:12 -0700

    Add Metadata
    Metadata now tracks map outputs

---
+++ src/MetadataHandler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Data;
+using System.Text;
+using Landis.Library.Metadata;
+using Landis.Core;
+using Edu.Wisc.Forest.Flel.Util;
+using System.IO;
+using Flel = Edu.Wisc.Forest.Flel;
+
+namespace Landis.Extension.Output.MaxSpeciesAge
+{
+    public static class MetadataHandler
+    {
+
+        public static ExtensionMetadata Extension { get; set; }
+        public static void InitializeMetadata(string speciesNameTemplate, IEnumerable<ISpecies> selectedSpecies)
+        {
+
+            ScenarioReplicationMetadata scenRep = new ScenarioReplicationMetadata()
+            {
+                RasterOutCellArea = PlugIn.ModelCore.CellArea,
+                TimeMin = PlugIn.ModelCore.StartTime,
+                TimeMax = PlugIn.ModelCore.EndTime,
+            };
+
+            Extension = new ExtensionMetadata(PlugIn.ModelCore)
+            //Extension = new ExtensionMetadata()
+            {
+                Name = PlugIn.ExtensionName,
+                TimeInterval = PlugIn.ModelCore.CurrentTime,
+                ScenarioReplicationMetadata = scenRep
+            };
+
+            //---------------------------------------            
+            //          map outputs:         
+            //---------------------------------------
+
+            foreach(ISpecies species in selectedSpecies)
+            {
+                OutputMetadata mapOut_max = new OutputMetadata()
+                {
+                    Type = OutputType.Map,
+                    Name = "max_age_" + species.Name,
+                    FilePath = MapNameTemplates.ReplaceTemplateVars(speciesNameTemplate, species.Name, PlugIn.ModelCore.CurrentTime),
+                    Map_DataType = MapDataType.Continuous,
+                    Visualize = true,
+                    //Map_Unit = "categorical",
+                };
+                Extension.OutputMetadatas.Add(mapOut_max);
+            }
+
+            OutputMetadata mapOut_max_all = new OutputMetadata()
+            {
+                Type = OutputType.Map,
+                Name = "AllSppMaxAge",
+                FilePath = MapNameTemplates.ReplaceTemplateVars(speciesNameTemplate, "AllSppMaxAge", PlugIn.ModelCore.CurrentTime),
+                Map_DataType = MapDataType.Continuous,
+                Visualize = true,
+                //Map_Unit = "categorical",
+            };
+            Extension.OutputMetadatas.Add(mapOut_max_all);
+
+
+
+            //---------------------------------------
+            MetadataProvider mp = new MetadataProvider(Extension);
+            mp.WriteMetadataToXMLFile("Metadata", Extension.Name, Extension.Name);
+        }
+        public static void CreateDirectory(string path)
+        {
+            //Require.ArgumentNotNull(path);
+            path = path.Trim(null);
+            if (path.Length == 0)
+                throw new ArgumentException("path is empty or just whitespace");
+
+            string dir = Path.GetDirectoryName(path);
+            if (!string.IsNullOrEmpty(dir))
+            {
+                Flel.Util.Directory.EnsureExists(dir);
+            }
+
+            //return new StreamWriter(path);
+            return;
+        }
+    }
+}
