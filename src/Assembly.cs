06866bec17eab8cc7682a1cec4eb20faa5b18a50
Kelsey Ruckert <klr324@rsc64dot1x-52.ems.psu.edu>
2017-12-12 16:59:15 -0500

    Update to .netstandard 2.0; use nuget packages

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -1,17 +1,2 @@
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Landis.Extension.Output-Maximum-Spp-Age")]
-
-[assembly: AssemblyDescription("Extension Output-Maximum-Spp-Age")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("2.*")]
